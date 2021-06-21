87251a2cad43f2de82b0869fae3b20b2087ba62e
Josh Renaud <josh.renaud41@gmail.com>
2021-06-21 14:12:00 -0500

    re-worked js code to function correctly, needed to call function and append correctly

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,27 @@
 const currentDate = moment().format('dddd MMMM Do, YYYY');
 const currentTime = moment().format('hh:mm:ss a');
 const currentHour = moment().format('HH');
-
-$("#currentDay").text(currentDate);
-
 let amPM = "AM";
-let lastHour = " ";
-let timeMap = newMap();
+let lastHour = "";
+let timeMap = new Map();
+
+$('#currentDay').text(currentDate);
 
 
-//.. Local storage retrieve after storing on bottom of code
+
 if (localStorage.getItem("myMap")) {
-    timeMap = newMap(JSON.parse(localStorage.myMap));
+    timeMap = new Map(JSON.parse(localStorage.myMap));
 
 } else {
-    let timeMap = newMap();
+    let timeMap = new Map();
 }
+
 
 
 for (let hour = 9; hour < 18; hour++) {
 
     let timeBlock = $('<div>');
+
 
     if (hour < 12) {
         amPM = "AM";
@@ -29,57 +30,62 @@ for (let hour = 9; hour < 18; hour++) {
     }
 
     let timeDiv = $('<div>');
-
     if (hour > 12) {
         lastHour = hour - 12;
     } else {
         lastHour = hour;
     }
 
-    timeBlock.addClass('time-block');
-    timeBlock.text(lastHour + amPM);
+    if (lastHour < 10) {
+        lastHour = "  " + lastHour;
+    }
 
-    
+    timeDiv.text(lastHour + amPM);
+    timeDiv.addClass('time-div');
+
     let descriptionDiv = $("<div>");
     let textAreaForDiv = $("<textarea>");
     textAreaForDiv.attr('id', 'textarea' + hour);
 
     descriptionDiv.append(textAreaForDiv);
     descriptionDiv.addClass("description");
-
+    descriptionDiv.css("width", "75%");
 
     let saveIcon = $('<i>');
     saveIcon.addClass("fa fa-save");
     let saveDiv = $("<div>");
     saveDiv.addClass("saveBtn ");
     saveDiv.attr('id', hour);
-    saveDiv.append(saveIcon);
+
 
     
+    saveDiv.append(saveIcon);
     timeBlock.append(timeDiv, descriptionDiv, saveDiv);
+    timeBlock.addClass("time-block row");
 
     if (currentHour > hour) {
-
         
         timeBlock.addClass("past");
 
     } else if (currentHour < hour) {
 
-        
         timeBlock.addClass("future");
         textAreaForDiv.attr("placeholder", "Enter a task to complete this hour...");
 
     } else {
-     
+
         timeBlock.addClass("present");
-        textAreaForDiv.attr("placeholder", "Add calendar event here");
+        textAreaForDiv.attr("placeholder", "Enter activity for calendar slot");
     }
 
-    $("#container").append(timeBlock);
+    
+    $("#scheduleblock").append(timeBlock);
 
 
 
 }
+
+
 
 timeMap.forEach(function (text, key) {
 
@@ -88,11 +94,13 @@ timeMap.forEach(function (text, key) {
 
 });
 
+
+
+
 $(".saveBtn").on('click', function () {
 
     let textAreaVar = "#textarea" + (this.id);
-
-    timeMap.set((this.id), document.querySelector(textAreaVar).value);
+    timeMap.set((this.id), document.querySelector(textAreaVar).value);  
     localStorage.myMap = JSON.stringify(Array.from(timeMap.entries()));
 
 
